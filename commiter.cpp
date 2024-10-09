bda56a8b097c40982a2c4fe6ba32361a40a38d37
WrongAnswer90 <wangyice1537@163.com>
2024-10-09 20:49:20 +0800

    Updated on 2024-10-09 20:49

--- commiter.cpp
+++ commiter.cpp
@@ -6,24 +6,18 @@ inline void commit()
 {
 	char s[210];
 	time_t tmp;
-	//ababa
 	time(&tmp);
 	struct tm *p;
 	p=localtime(&tmp);
 	sprintf(s,"git commit -m \"Updated on %d-%02d-%02d %02d:%02d\"",1900+p->tm_year,
 	1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min);
 	
-	int x=system("git add .");
-	int y=system(s);
+	system("git add .");
+	system(s);
 	int z;
 	again:
 	z=system("git push -f origin main");
-	cerr<<x<<" "<<y<<" "<<z<<endl;
-	if(z)
-	{
-		Sleep(20000);
-		goto again;
-	}
+	if(z){Sleep(20000);goto again;}
 }
 int len;
 int len2;
